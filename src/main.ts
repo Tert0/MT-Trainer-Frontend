9c8e954c3aa5b8f572b4f7ceee8ace7d24e1c7d5
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-04-13 12:17:42 +0200

    Removed Bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/tailwind.css';
-//import "bootstrap";
-//import "bootstrap/dist/css/bootstrap.min.css";
 
 let app = createApp(App);
 app.use(router);
